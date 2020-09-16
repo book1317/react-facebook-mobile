432079253a337e117715530af5e38110defd6e5c
book1317 <bookboks@hotmail.com>
2020-09-17 02:53:23 +0700

    goto proflie page on comment image click

--- src/page/HomePage/Post/Comment.tsx
+++ src/page/HomePage/Post/Comment.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { FaRegThumbsUp } from 'react-icons/fa'
 import css from './postStyle.module.scss'
+import history from 'utils/History'
 import { IComment } from 'store/PostStore.d'
 import { initProfile, IProfile, IProfileStore } from 'store/ProfileStore.d'
 import profileImage from 'image/profile1.png'
@@ -48,6 +49,11 @@ export default class Comment extends React.Component<
     this.setState({ isLike: !isLike })
   }
 
+  gotoOtherProfile = () => {
+    const ownerId = this.state.ownerProfile.id
+    history.push('/profile/' + ownerId)
+  }
+
   render() {
     const { content } = this.props.comment
     const { like, ownerProfile } = this.state
@@ -57,6 +63,7 @@ export default class Comment extends React.Component<
           alt=""
           src={ownerProfile.image || profileImage}
           className={`${css.profileImage} ${css.circleContainer} ${css.mini}`}
+          onClick={this.gotoOtherProfile}
         />
         <span className={css.commentText}>{content}</span>
         <div className={css.commentOptionContainer}>
